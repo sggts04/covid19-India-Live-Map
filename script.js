bccd28823bf8aa05846edd027aadf1e989e10554
Shreyas Gupta <technology.shreyas@gmail.com>
2020-03-16 17:40:16 +0530

    Add spheres according to number of corona cases with hovering

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let myMap;
 let canvas;
+let stats;
+let coords;
 const mappa = new Mappa('Leaflet');
 
 // Lets put all our map options in a single object
 const options = {
-    lat: 20.5937,
-    lng: 78.9629,
+    lat: 22,
+    lng: 82,
     zoom: 5,
     style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
 }
@@ -22,6 +24,7 @@ function preload() {
 
                 // Examine the text in the response
                 response.json().then(function (data) {
+                    stats = data;
                     console.log(data);
                 });
             }
@@ -29,6 +32,7 @@ function preload() {
         .catch(function (err) {
             console.log('Fetch Error :-S', err);
         });
+    coords = loadJSON("coords.json");
 }
 
 function setup() {
@@ -36,10 +40,31 @@ function setup() {
 
     myMap = mappa.tileMap(options);
     myMap.overlay(canvas);
+    console.log(coords);
 }
 
 function draw() {
+    clear();
+    if(stats) {
+        for(let st in coords) {
+            let cState = coords[st];
+            let stateName = cState.stateN;
+            let sState = stats.stateData[stateName];
+            if(sState) {
+                let cases = sState.cases;
+                let diameter = map(cases, 0, 50, 0, 8) * pow(2, myMap.zoom());
+                const pix = myMap.latLngToPixel(cState.latitude, cState.longitude);
 
+                var d = dist(mouseX, mouseY, pix.x, pix.y);
+                if (d < (diameter/2)) {
+                    fill(237, 138, 24, 200);
+                } else {
+                    fill(200, 100, 100, 100);
+                }
+                ellipse(pix.x, pix.y, diameter, diameter);
+            }
+        }
+    }
 }
 
 function windowResized() {
